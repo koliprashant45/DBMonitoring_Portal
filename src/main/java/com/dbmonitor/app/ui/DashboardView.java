3bdb1d043b009cbcfbfbc2a617c9b9f9ea415e56
Prashant Koli <koliprashant045@gmail.com>
2025-03-04 09:36:07 +0530

    ui: Implemented DashboardView class

--- src/main/java/com/dbmonitor/app/ui/DashboardView.java
+++ src/main/java/com/dbmonitor/app/ui/DashboardView.java
@@ -1,5 +1,33 @@
 package com.dbmonitor.app.ui;
 
-public class DashboardView {
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import jakarta.annotation.security.PermitAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@PageTitle("DB Monitoring Portal")
+@Route(value = "dashboard", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@PermitAll
+public class DashboardView extends VerticalLayout {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public DashboardView() {
+        setSpacing(false);
+
+        H2 header = new H2("DB Monitoring Portal");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
 }
